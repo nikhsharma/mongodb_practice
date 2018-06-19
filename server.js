cc25bf3f565c5c9f6f18673158a7733daf59b413
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-19 15:45:34 +0100

    initial commit. Can save new test object to db

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const parser = require('body-parser');
+const app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("mongo_practice");
+  console.log('Connected to database');
+
+  app.post('/api/stuff', function(req, res, next) {
+    const collectionOfStuff = db.collection('stuff');
+    const newThing = req.body;
+    collectionOfStuff.save(newThing, function(err, result) {
+      if(err) next(err);
+      res.status(201);
+      res.json(result.ops[0]);
+      console.log('Saved to database!');
+    })
+  });
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+
+})
